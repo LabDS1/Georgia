b814291c163d277ff707f2c90ea621501805e52a
Ullash_Vaghela <ullash.vaghela@brainvire.com>
2022-01-11 19:17:12 +0530

    [UPD] Account move: resolve create invoice

--- bv_v15_labds/models/sale.py
+++ bv_v15_labds/models/sale.py
@@ -2,6 +2,7 @@
 
 from odoo import fields, api, models, _
 from odoo.tools import html_keep_url, is_html_empty
+from datetime import datetime
 
 
 class SaleOrder(models.Model):
@@ -46,6 +47,57 @@ class SaleOrder(models.Model):
                 default_team_id=self.partner_id.team_id.id
             )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)], user_id=user_id)
         self.update(values)
+
+
+class MailTracking(models.Model):
+    _inherit = "mail.tracking.value"
+
+    @api.model
+    def create_tracking_values(self, initial_value, new_value, col_name, col_info, tracking_sequence, model_name):
+        tracked = True
+
+        field = self.env['ir.model.fields']._get(model_name, col_name)
+        if not field:
+            return
+
+        values = {'field': field.id, 'field_desc': col_info['string'], 'field_type': col_info['type'],
+                  'tracking_sequence': tracking_sequence}
+
+        if col_info['type'] in ['integer', 'float', 'char', 'text', 'datetime', 'monetary']:
+            values.update({
+                'old_value_%s' % col_info['type']: initial_value,
+                'new_value_%s' % col_info['type']: new_value
+            })
+        elif col_info['type'] == 'date':
+            values.update({
+                'old_value_datetime': initial_value and fields.Datetime.to_string(
+                    datetime.combine(fields.Date.from_string(initial_value), datetime.min.time())) or False,
+                'new_value_datetime': new_value and fields.Datetime.to_string(
+                    datetime.combine(fields.Date.from_string(new_value), datetime.min.time())) or False,
+            })
+        elif col_info['type'] == 'boolean':
+            values.update({
+                'old_value_integer': initial_value,
+                'new_value_integer': new_value
+            })
+        elif col_info['type'] == 'selection':
+            values.update({
+                'old_value_char': initial_value or '',
+                'new_value_char': new_value and dict(col_info['selection'])[new_value] or ''
+            })
+        elif col_info['type'] == 'many2one':
+            values.update({
+                'old_value_integer': initial_value and initial_value.id or 0,
+                'new_value_integer': new_value and new_value.id or 0,
+                'old_value_char': initial_value and initial_value.sudo().name_get()[0][1] or '',
+                'new_value_char': new_value and new_value.sudo().name_get()[0][1] or ''
+            })
+        else:
+            tracked = False
+
+        if tracked:
+            return values
+        return {}
 
 
 
