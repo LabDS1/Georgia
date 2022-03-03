e79783ce07124f29fa3c21dd2870c4936de2df67
Prasun Singh <prasun.singh@brainvire.com>
2022-03-03 19:08:06 +0530

    [Add] worked on task form and its sharing view

--- bv_v15_labds/models/project.py
+++ bv_v15_labds/models/project.py
@@ -4,5 +4,11 @@ from odoo import models, api, fields, _
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    project_description = fields.Html(related='project_id.description',string='Project Description',store=True,)
+    project_description = fields.Html(compute='_project_description',string='Project Description')
+
+    def _project_description(self):
+        if self.project_id:
+            self.project_description = self.project_id.description
+        else:
+            self.project_description = False
 
