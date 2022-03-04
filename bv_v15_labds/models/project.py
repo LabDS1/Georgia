3278fda7efed8f2f224fc2bc5bfe2a5cd2f6469c
Prasun Singh <prasun.singh@brainvire.com>
2022-03-04 14:28:23 +0530

    [Add] worked on task form and its sharing view

--- bv_v15_labds/models/project.py
+++ bv_v15_labds/models/project.py
@@ -1,59 +1,7 @@
 # -*- coding: utf-8 -*-
-
-from odoo import api, Command, fields, models, tools, SUPERUSER_ID, _
-
-
-
-PROJECT_TASK_READABLE_FIELDS = {
-    'id',
-    'active',
-    'description',
-    'priority',
-    'kanban_state_label',
-    'project_id',
-    'display_project_id',
-    'color',
-    'partner_is_company',
-    'commercial_partner_id',
-    'allow_subtasks',
-    'subtask_count',
-    'child_text',
-    'is_closed',
-    'email_from',
-    'create_date',
-    'write_date',
-    'company_id',
-    'displayed_image_id',
-    'display_name',
-    'portal_user_names',
-    'legend_normal',
-    'legend_blocked',
-    'legend_done',
-    'project_description',
-}
+from odoo import models, api, fields, _
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
     project_description = fields.Html(related='project_id.description',string='Project Description')
-
-    @property
-    def SELF_READABLE_FIELDS(self):
-        return PROJECT_TASK_READABLE_FIELDS | self.SELF_WRITABLE_FIELDS
-
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        fields = super().fields_get(allfields=allfields, attributes=attributes)
-        if not self.env.user.has_group('base.group_portal'):
-            return fields
-        readable_fields = self.SELF_READABLE_FIELDS
-        public_fields = {field_name: description for field_name, description in fields.items() if
-                         field_name in readable_fields}
-
-        writable_fields = self.SELF_WRITABLE_FIELDS
-        for field_name, description in public_fields.items():
-            if field_name not in writable_fields and not description.get('readonly', False):
-                # If the field is not in Writable fields and it is not readonly then we force the readonly to True
-                description['readonly'] = True
-
-        return public_fields
