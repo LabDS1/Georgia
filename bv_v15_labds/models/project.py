841d448b733b596edd0052b28b5584d7a125f696
Ullash_Vaghela <ullash.vaghela@brainvire.com>
2022-03-07 16:55:54 +0530

    [ADD] temp comment

--- bv_v15_labds/models/project.py
+++ bv_v15_labds/models/project.py
@@ -1,47 +1,47 @@
 # -*- coding: utf-8 -*-
 from odoo import models, api, fields, _
-
-PROJECT_TASK_READABLE_FIELDS = {
-    'id',
-    'active',
-    'description',
-    'priority',
-    'kanban_state_label',
-    'project_id',
-    'display_project_id',
-    'color',
-    'partner_is_company',
-    'commercial_partner_id',
-    'allow_subtasks',
-    'subtask_count',
-    'child_text',
-    'is_closed',
-    'email_from',
-    'create_date',
-    'write_date',
-    'company_id',
-    'displayed_image_id',
-    'display_name',
-    'portal_user_names',
-    'legend_normal',
-    'legend_blocked',
-    'legend_done',
-    'user_ids',
-}
-
-PROJECT_TASK_WRITABLE_FIELDS = {
-    'name',
-    'partner_id',
-    'partner_email',
-    'date_deadline',
-    'tag_ids',
-    'sequence',
-    'stage_id',
-    'kanban_state',
-    'child_ids',
-    'parent_id',
-    'priority',
-}
+#
+# PROJECT_TASK_READABLE_FIELDS = {
+#     'id',
+#     'active',
+#     'description',
+#     'priority',
+#     'kanban_state_label',
+#     'project_id',
+#     'display_project_id',
+#     'color',
+#     'partner_is_company',
+#     'commercial_partner_id',
+#     'allow_subtasks',
+#     'subtask_count',
+#     'child_text',
+#     'is_closed',
+#     'email_from',
+#     'create_date',
+#     'write_date',
+#     'company_id',
+#     'displayed_image_id',
+#     'display_name',
+#     'portal_user_names',
+#     'legend_normal',
+#     'legend_blocked',
+#     'legend_done',
+#     'user_ids',
+# }
+#
+# PROJECT_TASK_WRITABLE_FIELDS = {
+#     'name',
+#     'partner_id',
+#     'partner_email',
+#     'date_deadline',
+#     'tag_ids',
+#     'sequence',
+#     'stage_id',
+#     'kanban_state',
+#     'child_ids',
+#     'parent_id',
+#     'priority',
+# }
 
 
 class ProjectTask(models.Model):
@@ -49,8 +49,8 @@ class ProjectTask(models.Model):
 
     project_description = fields.Html(related='project_id.description',string='Project Description', store=True)
 
-    @property
-    def SELF_READABLE_FIELDS(self):
-        if 'project_description' in PROJECT_TASK_READABLE_FIELDS:
-            PROJECT_TASK_READABLE_FIELDS.remove('project_description')
-        return PROJECT_TASK_READABLE_FIELDS | self.SELF_WRITABLE_FIELDS
+    # @property
+    # def SELF_READABLE_FIELDS(self):
+    #     if 'project_description' in PROJECT_TASK_READABLE_FIELDS:
+    #         PROJECT_TASK_READABLE_FIELDS.remove('project_description')
+    #     return PROJECT_TASK_READABLE_FIELDS | self.SELF_WRITABLE_FIELDS
