8c2799706ca69d6af9f5a5835af576a9c5d2a009
Ullash_Vaghela <ullash.vaghela@brainvire.com>
2022-03-10 18:27:15 +0530

    [UPD] Add Groups in field

--- bv_v15_labds/models/project.py
+++ bv_v15_labds/models/project.py
@@ -5,4 +5,4 @@ from odoo import models, api, fields, _
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    project_description = fields.Html(related='project_id.description', string='Project Description', store=True)
+    project_description = fields.Html(related='project_id.description', string='Project Description', store=True, groups="base.group_portal,base.group_user")
