1477ca06c5f6e4800b9c23566d7a537bc1c9e590
Prasun Singh <prasun.singh@brainvire.com>
2022-03-24 14:26:06 +0530

    [ADD] added a html field on task view

--- bv_v15_labds/models/project.py
+++ bv_v15_labds/models/project.py
@@ -6,3 +6,4 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
 
     project_description = fields.Html(related='project_id.description', string='Project Description', store=True)
+    project_descrip = fields.Html(string='Project Description')
