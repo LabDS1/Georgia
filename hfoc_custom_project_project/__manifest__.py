554fb704b1696e7413110c2818f5405c6207db0b
hfoc11 <hfoc@live.com>
2023-09-19 21:36:52 -0500

    new group total so - project

---
+++ hfoc_custom_project_project/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HFoc - Custom Project',
+    'version': '15.0',
+    'category': 'Survey',
+    'summary': 'Custom Project',
+    'license':'LGPL-3',
+    'description': "Custom Project",
+    'author' : 'HFoc',
+    'depends': ['project','project_enterprise','sale_project','base'],
+    'data': [
+        'views/project_project.xml',
+    ],
+    "installable": True,
+    "application": False,
+
+}
