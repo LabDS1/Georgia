dc4d70b3b647cd9163ab019503acbcfa6d683977
Joel Tovar <Joel.Tovar@captivea.us>
2024-01-10 16:33:13 -0500

    create fields

---
+++ cap_sale_options/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '',
+    'category': 'Accounting',
+    'version': '1.0.1',
+    'author' : 'Almighty Consulting Solutions Pvt. Ltd.',
+    'support': 'info@almightycs.com',
+    'website': 'https://www.almightycs.com',
+    'license': 'OPL-1',
+    'summary': """Invoice Withholding management for Projects or Retainage Management.""",
+    'description': """Invoice Withholding management for Projects or Retainage Management.""",
+    'depends': ['base', 'sale_management'],
+    'data': [
+        # 'views/withholding_view.xml',
+    ],
+    'installable': True,
+    
+}
