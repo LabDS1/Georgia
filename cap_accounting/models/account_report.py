bfafe18552833d1d9e49e03b1466aaef44fcefcf
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2024-02-08 16:24:46 -0500

    [ADD] accounting report reformat

---
+++ cap_accounting/models/account_report.py
@@ -0,0 +1,152 @@
+from odoo import fields, api, models, _
+import ast
+import copy
+import datetime
+import io
+import json
+import logging
+import markupsafe
+from collections import defaultdict
+from math import copysign, inf
+
+import lxml.html
+from babel.dates import get_quarter_names
+from dateutil.relativedelta import relativedelta
+from markupsafe import Markup
+
+from odoo import models, fields, api, _
+from odoo.addons.web.controllers.main import clean_action
+from odoo.exceptions import RedirectWarning
+from odoo.osv import expression
+from odoo.tools import config, date_utils, get_lang
+from odoo.tools.misc import formatLang, format_date
+from odoo.tools.misc import xlsxwriter
+
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+class AccountReport(models.AbstractModel):
+    _inherit = "account.report"
+
+    def get_xlsx(self, options, response=None):
+        self = self.with_context(self._set_context(options))
+        output = io.BytesIO()
+        workbook = xlsxwriter.Workbook(output, {
+            'in_memory': True,
+            'strings_to_formulas': False,
+        })
+        sheet = workbook.add_worksheet(self._get_report_name()[:31])
+
+        date_default_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2, 'num_format': 'yyyy-mm-dd'})
+        date_default_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'num_format': 'yyyy-mm-dd'})
+        default_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2})
+        default_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666'})
+        title_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'bottom': 2})
+        level_0_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 6, 'font_color': '#666666'})
+        level_1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 1, 'font_color': '#666666'})
+        level_2_col1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666', 'indent': 1})
+        level_2_col1_total_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666'})
+        level_2_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666'})
+        level_3_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2})
+        level_3_col1_total_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666', 'indent': 1})
+        level_3_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666'})
+
+        #Set the first column width to 50
+        sheet.set_column(0, 0, 50)
+
+        y_offset = 0
+        headers, lines = self.with_context(no_format=True, print_mode=True, prefetch_fields=False)._get_table(options)
+
+        # Add headers.
+        for header in headers:
+            x_offset = 0
+            for column in header:
+                column_name_formated = column.get('name', '').replace('<br/>', ' ').replace('&nbsp;', ' ')
+                colspan = column.get('colspan', 1)
+                if colspan == 1:
+                    sheet.write(y_offset, x_offset, column_name_formated, title_style)
+                else:
+                    sheet.merge_range(y_offset, x_offset, y_offset, x_offset + colspan - 1, column_name_formated, title_style)
+                x_offset += colspan
+            y_offset += 1
+
+        if options.get('hierarchy'):
+            lines = self.with_context(no_format=True)._create_hierarchy(lines, options)
+        if options.get('selected_column'):
+            lines = self._sort_lines(lines, options)
+
+        # Add lines.
+        for y in range(0, len(lines)):
+            _logger.info(f"Line: {lines[y].get('name')}")
+            level = lines[y].get('level')
+            if lines[y].get('caret_options'):
+                style = level_3_style
+                col1_style = level_3_col1_style
+            elif level == 0:
+                y_offset += 1
+                style = level_0_style
+                col1_style = style
+            elif level == 1:
+                style = level_1_style
+                col1_style = style
+            elif level == 2:
+                style = level_2_style
+                col1_style = 'total' in lines[y].get('class', '').split(' ') and level_2_col1_total_style or level_2_col1_style
+            elif level == 3:
+                style = level_3_style
+                col1_style = 'total' in lines[y].get('class', '').split(' ') and level_3_col1_total_style or level_3_col1_style
+            else:
+                style = default_style
+                col1_style = default_col1_style
+
+            #write the first column, with a specific style to manage the indentation
+            cell_type, cell_value = self._get_cell_type_value(lines[y])
+            if cell_type == 'date':
+                sheet.write_datetime(y + y_offset, 0, cell_value, date_default_col1_style)
+            else:
+                sheet.write(y + y_offset, 0, cell_value, col1_style)
+
+            #write all the remaining cells
+            for x in range(1, len(lines[y]['columns']) + 1):
+                ### CUSTOM START ###
+                lines_to_change = [
+                        'Revenue',
+                        'Cost of Revenue',
+                        'Expenses',
+                        'Other Income',
+                        'Other Expenses',
+                        'Current Assets',
+                        'Bank and Cash Accounts',
+                        'Receivables',
+                        'Prepayments',
+                        'Other Current Assets',
+                        'Fixed Assets',
+                        'Non-current Assets',
+                        'LIABILITIES',
+                        'Current Liabilities',
+                        'Account Payables',
+                        'Other Current Liabilities',
+                        'Non-current Liabilities',
+                        'EQUITY',
+                        'Unallocated Earnings',
+                        'Current Year Unallocated Earnings',
+                        'Retained Earnings',
+                        'Capital',
+                        ]
+                ### CUSTOM END ###
+                cell_type, cell_value = self._get_cell_type_value(lines[y]['columns'][x - 1])
+                ### CUSTOM START ###
+                if lines[y].get('name') in lines_to_change:
+                    cell_value = ''
+                ### CUSTOM END ###
+                if cell_type == 'date':
+                    sheet.write_datetime(y + y_offset, x + lines[y].get('colspan', 1) - 1, cell_value, date_default_style)
+                else:
+                    sheet.write(y + y_offset, x + lines[y].get('colspan', 1) - 1, cell_value, style)
+
+        workbook.close()
+        output.seek(0)
+        generated_file = output.read()
+        output.close()
+
+        return generated_file
