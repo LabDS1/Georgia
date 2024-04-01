280a78328365b2d42313f70e00c2c217052e184e
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2024-04-01 18:16:27 -0400

    Update project_project.py

--- cap_sale_options/models/project_project.py
+++ cap_sale_options/models/project_project.py
@@ -13,11 +13,10 @@ class Project(models.Model):
         subtype_id = self.env.ref('project.mt_project_stage_change').id
         for record in self:
             if record.stage_id.id == done_stage_id:
-                messages = self.env['mail.message'].search([('subtype_id','=',subtype_id),
+                msg = self.env['mail.message'].search([('subtype_id','=',subtype_id),
                                                  ('res_id','=',record.id),
-                                                 ('model','=','project.project')])
-                
-                message = messages.filtered(lambda msg: msg.tracking_value_ids[0].new_value_integer == done_stage_id)
-                if message:
-                    record.move_to_done_stage = message.create_date
-                    
+                                                 ('model','=','project.project')],
+                                                 order="id desc",
+                                                 limit=1)
+                if msg.tracking_value_ids[0].new_value_integer == done_stage_id:
+                    record.move_to_done_stage = msg.create_date
