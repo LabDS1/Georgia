5ab9b76f2ffe18bb39054129597fbb29f0ea6e84
captivea-midhun <captivea-midhun@captivea.us>
2024-06-16 16:10:01 -0400

    [ADD] Enabled analytic account automation on quotation creation from opportunity + hid Taxes column on quotation/Optional products.

---
+++ cap_sales/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Captivea Sales',
+    'category': 'Sales',
+    'version': '1.0.1',
+    'author' : 'Captivea',
+    'support': 'captivea.com',
+    'website': 'https://www.captivea.com',
+    'license': 'OPL-1',
+    'summary': """Sales flow related customizations.""",
+    'description': """This module will house any customizations relevant to the sales workflow.""",
+    'depends': ['base', 'account', 'sale_crm'],
+    'data': [
+    ],
+    'installable': True,
+    
+}
